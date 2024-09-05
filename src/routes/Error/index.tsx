1f5af79f96908555c7c1dcc315778cb01456ad79
P3terHenry <pedro.antonieti@outlook.com>
2024-09-05 01:18:57 -0300

    Adicionado pasta de rota `Error` com index.tsx.

---
+++ src/routes/Error/index.tsx
@@ -0,0 +1,12 @@
+function Error(){
+
+    document.title = "Lele's Repairs - Erro";
+    
+    return (
+        <div>
+          <h1 className="text-3xl">Erro</h1>
+        </div>
+    );
+}
+
+export default Error;
