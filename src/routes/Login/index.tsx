f5d1e63a75d1a6a4c26ac52ce414af1efbd8437c
P3terHenry <pedro.antonieti@outlook.com>
2024-09-08 00:03:03 -0300

    Criação da estrutura da página de login com tags semánticas do HTML e componentes.

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -1,11 +1,36 @@
+import { Link } from "react-router-dom";
+import Checkmark from "../../components/Globais/Checkmark";
+import Input from "../../components/Globais/Input";
+import Logo from "../../components/Globais/Logo";
+
 function Login(){
 
     document.title = "Lele's Repairs - Login";
     
     return (
-        <div>
-          <h1 className="text-3xl">Login</h1>
-        </div>
+      <div className="w-screen h-screen flex flex-row justify-center items-center">
+        <main className="flex flex-row justify-center items-center drop-shadow-row">
+          <section className="w-[328px] h-full rounded-[30px] border-[2px] border-color_6 bg-color_2">
+            <div className="w-full h-full inline-flex flex-col items-center p-4 gap-8">
+              <Logo/>
+              <div className="w-72 h-full flex flex-col items-center gap-4">
+                <form className="w-72h-full flex flex-col items-center gap-3" action="/assistentes" method="get">
+                  <Input icon="bg-icon-email" classname={'w-72 h-12 p-3 pl-10 font-normal text-sm box-border rounded-full border-2 border-color_7 bg-color_3 focus:outline-color_9 drop-shadow-row placeholder-color_9 text-color_9'} type="email" placeholder="E-mail" name={"txtEmail"} id={'email'} required={true}/>
+                  <Input icon="bg-icon-password" classname={'w-72 h-12 p-3 pl-10 font-normal text-sm box-border rounded-full border-2 border-color_7 bg-color_3 focus:outline-color_9 drop-shadow-row placeholder-color_9 text-color_9'} type="password" placeholder="Senha" name={"txtSenha"} id={'senha'} minlength={8} maxlength={16} required={true}/>
+                  <Checkmark classname="fixed left-20 w-5 h-5 border-color_6 bg-color_7" type="checkbox" id="forgot-password" name="forgot-password"/>
+                  <button className="flex items-center justify-center gap-2.5 h-11 px-11 rounded-full border-2 border-color_6 bg-color_3 shadow-button font-semibold text-color_11 cursor-pointer hover:bg-color_4"id="button-submit" type="submit">Entrar</button>
+                  <Link className="font-normal leading-normal text-xs underline text-color_9" to="/forgot-password">Esqueci a senha?</Link>
+                  <Link className="font-normal leading-normal text-xs underline text-color_9" to="/cadastro">Primeiro acesso?</Link>
+                  <div className="h-8 justify-start items-center inline-flex">
+                    <div className="w-8 h-8 relative bg-icon-facebook bg-center"/>
+                    <div className="w-8 h-8 relative bg-icon-instagram bg-center"/>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </section>
+        </main>
+      </div>
     );
 }
 
