bcd7f46a325db65ed5b28d7813fd3f09699eb71b
P3terHenry <pedro.antonieti@outlook.com>
2024-09-11 12:12:02 -0300

    Inserido div interna com class para melhor estruturação do componente.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,9 @@ type HeaderProps = {
 function Header({children}: HeaderProps) {
     return (
         <header className="w-screen h-screen flex flex-row justify-center items-center">
-             {children}
+            <div className="flex flex-row justify-center items-center drop-shadow-row gap-5">
+                {children}
+            </div>
         </header>
     );
 }
