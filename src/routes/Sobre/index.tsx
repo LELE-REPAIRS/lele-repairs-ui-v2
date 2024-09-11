0ed204aa66efe959899693dd1efdf23fea137559
P3terHenry <pedro.antonieti@outlook.com>
2024-09-11 12:13:39 -0300

    Inserção da tag main e remoção da div principal que estrutura o body (foi inserida no componente `Header`).

--- src/routes/Sobre/index.tsx
+++ src/routes/Sobre/index.tsx
@@ -8,22 +8,23 @@ function Sobre(){
     
     return (
       <Header>
-        <div className="flex flex-row justify-center items-center drop-shadow-row">
-          <nav className="h-[900px] px-6 pt-6 pb-8 bg-color_2 rounded-[30px] border-2 border-color_6 flex-col justify-between items-start inline-flex">
-            <div className="self-stretch h-[402px] flex-col justify-start items-start gap-[52px] flex">
-              <Avatar src={'/avatar.png'} name={'Pedro Henrique Vasco Antonieti'} email={'rm556253@fiap.com.br'}/>
-              <div className="h-[300px] flex-col justify-start items-start gap-6 inline-flex">
-                <MenuList icon={'bg-icon-robot'} select={false} to={'/assistentes'} value="Assistentes"/>
-                <MenuList icon={'bg-icon-car'} select={false} to={'/meus-carros'} value="Meus Carros"/>
-                <MenuList icon={'bg-icon-conhecimento'} select={false} to={'/conhecimentos'} value="Conhecimentos"/>
-                <MenuList icon={'bg-icon-info'} select={true} to={'/sobre'} value="Sobre"/>
-              </div>
+        <nav className="h-[900px] px-6 pt-6 pb-8 bg-color_2 rounded-[30px] border-2 border-color_6 flex-col justify-between items-start inline-flex">
+          <div className="self-stretch h-[402px] flex-col justify-start items-start gap-[52px] flex">
+            <Avatar src={'/avatar.png'} name={'Pedro Henrique Vasco Antonieti'} email={'rm556253@fiap.com.br'}/>
+            <div className="h-[300px] flex-col justify-start items-start gap-6 inline-flex">
+              <MenuList icon={'bg-icon-robot'} select={false} to={'/assistentes'} value="Assistentes"/>
+              <MenuList icon={'bg-icon-car'} select={false} to={'/meus-carros'} value="Meus Carros"/>
+              <MenuList icon={'bg-icon-conhecimento'} select={false} to={'/conhecimentos'} value="Conhecimentos"/>
+              <MenuList icon={'bg-icon-info'} select={true} to={'/sobre'} value="Sobre"/>
             </div>
-            <div className="self-stretch h-14 flex-col justify-start items-start gap-2 flex">
-              <MenuList icon={'bg-icon-logout'} select={false} to={'/sair'} value="Sair"/>
-            </div>
-          </nav>
-        </div>
+          </div>
+          <div className="self-stretch h-14 flex-col justify-start items-start gap-2 flex">
+            <MenuList icon={'bg-icon-logout'} select={false} to={'/sair'} value="Sair"/>
+          </div>
+        </nav>
+        <main className="w-[1084px] h-[900px] bg-color_2 rounded-[30px] border-2 border-color_6">
+
+        </main>
       </Header>
     );
 }
