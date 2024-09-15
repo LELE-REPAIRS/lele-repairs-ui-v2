c8be9ba419c8a3f1a2f6e4f9dfcab20688060027
P3terHenry <pedro.antonieti@outlook.com>
2024-09-14 21:39:41 -0300

    Criado componente car.

---
+++ src/components/MeusCarros/Car.tsx
@@ -0,0 +1,28 @@
+type ChatProps = {
+    apelido: string,
+    marca: string,
+    modelo: string,
+    ano: string,
+    img_car: string,
+    km: number
+}
+
+
+function Chat({apelido, marca, modelo, ano, img_car, km}: ChatProps){
+    return (
+        <div className="h-[50px] justify-start items-start gap-2.5 inline-flex">
+            <img className="w-[50px] h-[50px] rounded-full" src={img_car}/>
+            <div className="w-[196px] flex-col justify-start items-start inline-flex gap-1">
+                <div className="self-stretch h-9 flex-col justify-start items-start flex">
+                    <p className="self-stretch h-5 text-color_11 text-[15px] font-bold">{apelido}</p>
+                    <p className="self-stretch h-4 text-color_9 text-[15px] font-normal">{marca}</p>
+                </div>
+                <div className="h-3.5 justify-end items-center inline-flex">
+                    <p className="w-60 h-3.5 text-black text-[10px] font-extralight leading-[14px]">{marca} - {modelo} - {ano} - {km}km rodados</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
